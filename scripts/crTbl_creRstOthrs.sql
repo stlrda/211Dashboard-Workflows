7bc476fbce01236b024335b4e9cbbeafd848217b
hbhatia-dghrty <64438763+hbhatia-dghrty@users.noreply.github.com>
2020-05-11 11:55:29 -0500

    Updated the table (structure) definition...
    Updated the table definition to include column DATA_SOURCE to reflect the staging table that the data in table CRE_COVID_DATA are sourced from.

--- scripts/crTbl_creRstOthrs.sql
+++ scripts/crTbl_creRstOthrs.sql
@@ -3,6 +3,13 @@
 --
 -- This is DDL to create Rest of the core tables.
 -- These core-tables pertain to the factual data for covid-19 and 211-help-line 
+--
+-------------------------------------------------------------------
+-- 2020-05-11 (Mon.) Haresh Bhatia.
+--
+-- 1. Included additional attribute DATA_SOURCE, in table CRE_COVID_DATA (see below)
+-- 2. The values of the attribute shall indicate the staging table the data are 
+--    collected from. [See the table comments for more details.]
 -- 
 --==================================================================================
 -- A. Table CRE_LAST_SUCCESS_RUN_DT
@@ -60,7 +67,8 @@ VALUES ('DLY_ALL', '1900-01-01')
 --
 CREATE TABLE 
           IF NOT EXISTS  uw211dashboard.public.cre_covid_data
-(report_date         DATE,
+(data_source         VARCHAR(15),
+ report_date         DATE,
  state_nm            VARCHAR(30),
  county_nm           VARCHAR(30),
  geo_id              VARCHAR(10),
@@ -76,14 +84,14 @@ CREATE TABLE
  case_fatality_rate  NUMERIC(12,6),
  created_tsp         TIMESTAMPTZ     NOT NULL DEFAULT now(),
  last_update_tsp     TIMESTAMPTZ     NOT NULL DEFAULT now(),
- PRIMARY KEY (report_date, state_nm, county_nm, geo_id)
+ PRIMARY KEY (data_source, report_date, state_nm, county_nm, geo_id)
 );
 
 
 COMMENT ON TABLE uw211dashboard.public.cre_covid_data IS
 'Table CRE_COVID_DATA contains data for the covid related attributes (cases, mortality, and respective rates).
 
-The data for this table was consolidated from the staging tables - STG_COVID_DLY_VIZ_CNTY_ALL, STG_COVID_ZIP_STL_CounTY, and STG_COVID_ZIP_STL_CiTY.
+The data for this table was consolidated from the staging tables - STG_COVID_DLY_VIZ_CNTY_ALL, STG_COVID_ZIP_STL_CounTY, and STG_COVID_ZIP_STL_CiTY; as indicated by the respective values of "ALL_COUNTY", "STL_COUNTY", and "STL_CITY" in the attribute DATA_SOURCE. [The attribute DATA_SOURCE is part of the PK.] 
 
 The data in STG_COVID_ZIP_STL_CounTY and ..._CiTY are (at least for present) limited only to CASE COUNT and RATE. [These data may get expanded to other attributes covered in STG_COVID_DLY_VIZ_CNTY_ALL.
 
