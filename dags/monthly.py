99506dfc66037ba414b0515f991f94d3ec803ba0
Keenan Berry <keenanberry22@gmail.com>
2020-05-20 14:04:56 -0500

    commit changes for monthly dag config

--- dags/monthly.py
+++ dags/monthly.py
@@ -19,7 +19,7 @@ Monthly DAG
 2. Truncate monthly stats staging table
 3. Load current unemployment data to staging
 4. Transfer data from staging to core
-    a. "Filtered" by date
+    a. "Filtered" by date (see SQL script)
 5. Update monthly run success timestamp
 
 '''
@@ -29,7 +29,7 @@ AIRFLOW_HOME = os.environ['AIRFLOW_HOME']
 
 args = {
     'owner': '211dashboard',
-    'start_date': datetime(2020, 5, 19),  # change this
+    'start_date': datetime(2020, 5, 21),  # change this
     'concurrency': 1,
     'retries': 0,
     'depends_on_past': False,
@@ -45,42 +45,47 @@ dag = DAG(
 
 ''' Define monthly airflow operators. '''
 
+# file @ url contains latest 14 months of data
 scrape_unemployment_stats = PythonOperator(
     task_id='scrape_unemployment_stats',
     python_callable=scrape_transform,
     op_kwargs={
         'url': 'https://www.bls.gov/web/metro/laucntycur14.txt',
-        'filename': 'unemployment_stats_current.csv',
+        'filename': 'unemployment_stats.csv',
         'transformer': transform_unemployment_stats,
         'sep': '|'
     },
     dag=dag)
 
-# truncate_monthly_staging_tables = PostgresOperator(
-#     task_id='truncate_monthly_staging_tables', 
-#     sql='trnctTbls_mnthly.sql', 
-#     dag=dag) 
+truncate_monthly_staging_table = PostgresOperator(
+    task_id='truncate_monthly_staging_table', 
+    sql='trnctTbls_mnthly.sql', 
+    dag=dag) 
     
-# load_current_unemployment_stats = PythonOperator(
-#     task_id='load_current_unemployment_stats',
-#     python_callable=load_file,
-#     op_kwargs={
-#         'filename': 'unemployment_stats_current.csv',
-#         'table_name': 'stg_unemployment_stats',
-#         'sep': '|',
-#         'nullstr': ''
-#     },
-#     dag=dag)
+load_current_unemployment_stats_staging = PythonOperator(
+    task_id='load_current_unemployment_stats_staging',
+    python_callable=load_file,
+    op_kwargs={
+        'filename': 'unemployment_stats.csv',
+        'table_name': 'stg_bls_unemployment_data_curr',
+        'sep': '|',
+        'nullstr': ''
+    },
+    dag=dag)
 
-# unemployment_stats_staging_to_core = PostgresOperator(
-#     task_id='unemployment_stats_staging_to_core', 
-#     sql='dtaMgrtn_unemployment_stats_mnthly.sql', 
-#     dag=dag) 
+unemployment_stats_staging_to_core = PostgresOperator(
+    task_id='unemployment_stats_staging_to_core', 
+    sql='dtaMgrtn_unemplStats_mnthly.sql', 
+    dag=dag)
 
-scrape_unemployment_stats
+update_monthly_timestamp = PostgresOperator(
+    task_id='update_monthly_timestamp', 
+    sql='setLstSccssflRnDt_mnthlyAll.sql', 
+    dag=dag) 
 
-# chain(scrape_unemployment_stats, 
-#       truncate_monthly_staging_tables, 
-#       load_current_unemployment_stats, 
-#       unemployment_stats_staging_to_core
-# )
+chain(scrape_unemployment_stats, 
+      truncate_monthly_staging_table, 
+      load_current_unemployment_stats_staging, 
+      unemployment_stats_staging_to_core,
+      update_monthly_timestamp
+)
