501c3f09db98caf42d7e2cad9bf73dfda9c78fb3
bransonf <branson.fox@slu.edu>
2020-07-09 16:38:45 -0500

    syntax

--- scripts/sql/crVu_creVuCnssDta.sql
+++ scripts/sql/crVu_creVuCnssDta.sql
@@ -140,7 +140,7 @@ AS
          est_tot_pop,
          imu_score,
          rpl_themes_svi_ndx,
-         area_sq_mi,
+         area_sq_mi
          --created_tsp,
          --last_update_tsp
    FROM  cre_census_data_by_county_yr2018
